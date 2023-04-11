277635f32423510c6994f63e0d77e4c524b34742
Anastasia Yakovleva <126901802+AnastasiaYakovleva-qa@users.noreply.github.com>
2023-04-11 22:20:56 +0500

    Add files via upload

---
+++ huntingpony.cy.js
@@ -0,0 +1,20 @@
+describe("Тестирование страницы магазина", function () {
+  it("Проверка покупки товара", function () {
+    cy.visit("https://huntingpony.com/");
+    cy.get(
+      '[data-index="2"] > .header__collections-controls > .header__collections-link'
+    ).click();
+    cy.get(
+      '[data-product-id="338932768"] > .product-preview__content > .product-preview__area-photo > .product-preview__photo > .img-ratio > .img-ratio__inner > a > :nth-child(1) > .product-preview__img-1'
+    ).click();
+    cy.wait(2000);
+    cy.get(".add-cart-counter__btn").click();
+    cy.wait(2000);
+    cy.get('[data-add-cart-counter-plus=""]').click();
+    cy.wait(2000);
+    cy.get(".add-cart-counter__detail").click();
+    cy.wait(2000);
+    cy.get(".cart-controls > .button").click();
+    cy.contains("Оформление заказа");
+  });
+});
